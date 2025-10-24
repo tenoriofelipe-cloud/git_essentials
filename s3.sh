cd4c887b594ffb8e02551c89b3dc443981038f1b
tenoriofelipe-cloud <tenorio.felipe@outlook.com>
2025-10-24 18:58:16 -0300

    Create s3.sh

---
+++ s3.sh
@@ -0,0 +1,8 @@
+function envio_s3()
+echo "Fazendo envio para o
+echo " Iniciando envio ... "
+echo " Envio finalizado"
+#generate an aws cli to sync my build folder to s3
+aws s3 sync ./build/ s3://bia-ftenorio
+
+s3 ... "
