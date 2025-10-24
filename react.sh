645709babff57b4f7660b896406594f5fe2f9568
tenoriofelipe-cloud <tenorio.felipe@outlook.com>
2025-10-24 19:20:23 -0300

    Update react.sh

--- react.sh
+++ react.sh
@@ -2,7 +2,7 @@ function build() {
 cd bia
 npm install
 echo " Iniciando build ... "
-VITE_API_URL=http://32.239.29.1 npm run build
+NODE_OPTIONS=--openssl-lgeacy-procider REACT_APP_API_URL=http://34.239.240.133 SKIP_PREFLIGHT_CHECK=true npm run build -prefix client
 echo " Build finalizado"
 }
 
